111ba88814e924aa73d02343735765d8ff6f0969
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-21 08:56:17 +0100

    change list of categories to list of all jokes

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { switchMap, map  } from 'rxjs/operators';
+import {forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  constructor(private http: HttpClient) { }
-  getData(): Observable<any> {
-    return this.http.get<string[]>("https://api.chucknorris.io/jokes/categories");
+  constructor(private http: HttpClient) { 
   }
-
+ getData(): Observable<any> {
+  return this.http.get<string[]>('https://api.chucknorris.io/jokes/categories').pipe(
+      switchMap((categories: string[]) => {
+        const requests = categories.map((category: string) => {
+          return this.http.get<any>(`https://api.chucknorris.io/jokes/search?query=${category}`);
+        });
+        return forkJoin(requests);
+      }),
+      map((responses: any[]) => {
+        const jokes = responses.reduce((acc: string[], response: any) => {
+          response.result.forEach((result: any) => {
+            acc.push(result.value);
+          });
+          return acc;
+        }, []);
+        return jokes;
+      })
+    );
+  }
 }
